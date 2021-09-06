36ff6728b7e5d200b22a151854e5fba26b194bf8
DaniP <80088420+danisa-loop@users.noreply.github.com>
2021-09-06 03:18:55 -0300

    createconfig

---
+++ create_config.sh
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+mkdir -p ~/.streamlit/
+echo "[server]"  > ~/.streamlit/config.toml 
+echo "headless = true"  >> ~/.streamlit/config.toml
+echo "port = $PORT"  >> ~/.streamlit/config.toml
+echo "enableCORS = false"  >> ~/.streamlit/config.toml
